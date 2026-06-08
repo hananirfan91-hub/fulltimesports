1f241086389783110c10d4980b4cb0bb4bb2f681
hananirfan91-hub <hananirfan91@gmail.com>
2026-06-08 16:59:23 +0500

    Update YouTube embed URL in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -667,7 +667,7 @@ export default function Home({ onNavigate, activeGeo }: HomeProps) {
             <div className="lg:col-span-2 flex flex-col justify-between">
               <div className="aspect-video bg-[#01140f] border border-emerald-850 rounded-2xl overflow-hidden relative shadow-2xl flex-grow">
                 <iframe 
-                  src={`https://youtu.be/yhOiwTVx9QY?si=88VT7eZW6N85N1RJ`}
+                  src={`https://www.youtube.com/embed/yhOiwTVx9QY?si=EnQLXHEtfZJ4yEhr`}
                   title="Exclusive Telemetry Analysis"
                   className="w-full h-full object-cover"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
