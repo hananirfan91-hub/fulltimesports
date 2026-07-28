e6c65ebff1ba131fa05709ea4dd352fceb66c885
hananirfan91-hub <hananirfan91@gmail.com>
2026-07-29 00:19:08 +0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -679,7 +679,7 @@ export default function Home({ onNavigate, activeGeo }: HomeProps) {
             <div className="lg:col-span-2 flex flex-col justify-between">
               <div className="aspect-video bg-[#01140f] border border-emerald-850 rounded-2xl overflow-hidden relative shadow-2xl flex-grow">
                 <iframe 
-                  src={`https://www.youtube.com/embed/${selectedVideoId}?autoplay=1&mute=1&playlist=${selectedVideoId}&loop=1&controls=1&modestbranding=1`}
+                  src="https://www.youtube.com/embed/MrQ5OIrtiSA?si=X5NiSqBiX_bAUw93"
                   title="Exclusive Telemetry Analysis"
                   className="w-full h-full object-cover"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
